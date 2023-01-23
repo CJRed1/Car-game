4f3d1bca3de73c2850c3b2838c3fea0024ab92ec
CJRed1 <88350408+CJRed1@users.noreply.github.com>
2023-01-23 17:46:58 -0400

    fake 3d, also no car

--- main.py
+++ main.py
@@ -15,29 +15,20 @@ screen = pg.display.set_mode(winSize)
 pg.display.set_caption("wow")
 pg.display.set_icon(pg.image.load('images/macrin.png'))
 
-# BG Color
+# Road
+
+# BG
 screen.fill((135, 255, 135))
+pg.draw.ellipse(screen, (50, 200, 50), (winWidth/4-25, -winHeight, winWidth/2+50, winHeight*3))
+pg.draw.rect(screen, (75, 75, 75), (winWidth/4, 0, winWidth/2, winHeight))
+pg.draw.line(screen, (255, 255, 255), (winWidth/4+10, 0), (winWidth/4+10, winHeight), 10)
+pg.draw.line(screen, (255, 255, 255), (winWidth/2+winWidth/4-12, 0), (winWidth/2+winWidth/4-12, winHeight), 10)
+pg.draw.line(screen, (255, 213, 0), (winWidth/2-5, 0), (winWidth/2-5, winHeight), 10)
 pg.display.update()
 
-class Car():
-    def __init__(self, winParent):
-        self.winParent = winParent
-        self.image = pg.image.load('images/carmf.png')
-        self.x = 400
-        self.y = 650
-        self.hspeed = 0
-
-    def draw(self):
-        self.winParent.fill((135, 255, 135))
-        self.winParent.blit(self.image, (self.x, self.y))
-        pg.display.flip()
-
-car = Car(screen)
 # Game Loop
 running = True
 while running:
-    car.draw()
-    car.x += car.hspeed
     for event in pg.event.get():
         if event.type == QUIT:
             running = False
@@ -45,29 +36,3 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-
-            if event.key == K_LEFT:
-                car.hspeed = -2
-            elif event.key == K_RIGHT:
-                car.hspeed = 2
-
-            if funnycolor == 0 and event.key == K_h:
-                funnycolor = 1
-            elif funnycolor == 1 and event.key == K_e:
-                funnycolor = 2
-            elif funnycolor == 2 and event.key == K_y:
-                if curBG == 0:
-                    screen.fill((255, 135, 255))
-                    curBG = 1
-                else:
-                    screen.fill((135, 255, 135))
-                    curBG = 0
-                pg.display.update()
-                funnycolor = 0
-            else:
-                funnycolor = 0
-
-        if event.type == pg.KEYUP:
-            if event.key == K_LEFT or event.key == K_RIGHT:
-                car.hspeed = 0
-    pg.display.update()
