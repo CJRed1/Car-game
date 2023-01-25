e4caea7dfb1c5ba183cbf045eb12d7b9e557d36e
CJRed1 <88350408+CJRed1@users.noreply.github.com>
2023-01-25 16:56:14 -0400

    e

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ pg.display.set_icon(pg.image.load('images/macrin.png'))
 
 # BG
 screen.fill((135, 255, 135))
+
 pg.draw.ellipse(screen, (50, 200, 50), (winWidth/4-25, -winHeight, winWidth/2+50, winHeight*3))
 pg.draw.rect(screen, (75, 75, 75), (winWidth/4, 0, winWidth/2, winHeight))
+
 pg.draw.line(screen, (255, 255, 255), (winWidth/4+10, 0), (winWidth/4+10, winHeight), 10)
 pg.draw.line(screen, (255, 255, 255), (winWidth/2+winWidth/4-12, 0), (winWidth/2+winWidth/4-12, winHeight), 10)
 pg.draw.line(screen, (255, 213, 0), (winWidth/2-5, 0), (winWidth/2-5, winHeight), 10)
+
 pg.display.update()
 
 # Game Loop
